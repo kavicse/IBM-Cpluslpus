757957a957172b9cebac19ed6b716d8f585d2985
kavicse <30829826+kavicse@users.noreply.github.com>
2018-03-11 14:54:29 +0530

    Create task31.c

---
+++ task31.c
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    public:
+    char name[20],g[20];
+    int m1,m2,m3,tot;
+    void getdata()
+    {
+        cout<<"Enter name\n";
+        cin>>name;
+        cout<<"Enter undergraduate or graduate";
+        cin>>g;
+        cout<<"Enter mark1\n";
+        cin>>m1;
+        cout<<"Enter mark2\n";
+        cin>>m2;
+        cout<<"Enter mark3\n";
+        cin>>m3;
+    }
+    void calculate()
+    {
+        if(g=="graduate")
+        {
+            tot=m1+m2+m3;
+            if(tot/3>=80)
+            cout<<name<<"\tpass!!!\n";
+            else
+            cout<<name<<"\tFail!!!\n";
+        }
+        else
+        {
+             tot=m1+m2+m3;
+            if(tot/3>=70)
+            cout<<name<<"\tpass!!!\n";
+            else
+            cout<<name<<"\tFail!!!\n";
+        }
+    }
+    
+     
+};
+int main()
+{
+    student s;
+    s.getdata();
+    s.calculate();
+    return 0;
+}
