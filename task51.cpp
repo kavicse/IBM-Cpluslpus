b221dbae6ef5f634ab7ed92c905d19c5037e8104
kavicse <30829826+kavicse@users.noreply.github.com>
2018-03-25 11:04:01 +0530

    Create task51.cpp

---
+++ task51.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+ 
+class calculated{
+   public:
+      void add(int i,int j) {
+        int s=i+j;
+        cout << "Integer: " << s << endl;
+      }
+      public:
+      void add(float f, float u) {
+        float k=f+u;
+        cout << "Float: " << k<< endl;
+      }
+      public:
+      void add(int a,int b,int c) {
+        int m=a+b+c;
+      
+        cout << "Integer: " << m << endl;
+      }
+      public:
+      void add(double r,double x,double z) {
+        double l=x+r+z;
+        cout << "Double: " << l<< endl;
+      }
+};
+
+int main(void) 
+{
+   int i=5,j=10,a=10,b=5,c=40;
+   float f=5.5,float u=4.0;
+   double r=10.00,x=67.0,z=89.0;
+   calculated p;
+   p.add(i,j);
+   p.add(f,u);
+   p.add(a,b,c);
+   p.add(r,x,z);
+   return 0;
+}
