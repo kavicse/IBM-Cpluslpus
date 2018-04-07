7c95b278b5a9bdf8167fd57d733fea0d3c10af90
kavicse <30829826+kavicse@users.noreply.github.com>
2018-04-07 11:06:40 +0530

    Create task61.cpp

---
+++ task61.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+	#include <string>
+	using namespace std;
+
+	int main ()
+	{
+		int num;
+		string str = "wrong number used";
+		cout << "Input 1 or 2: ";
+		cin >> num;
+
+		try
+		{
+			if ( num == 1 )
+			{
+				throw 5;
+			}
+			if ( num == 2 )
+			{
+				throw 1.1f;
+			}
+			if ( num != 1 || num != 2 )
+			{
+				throw str;
+			}
+		}
+
+		catch (int a)
+		{
+			cout << "An exception occurred!" << endl;
+			cout << "Exception number is: " << a << endl;
+		}
+		catch (float b)
+		{
+			cout << "An exception occurred!" << endl;
+			cout << "Exception number is: " << b << endl;
+		}
+		catch (...)
+		{
+			cout << "A default exception occurred!" << endl;
+			cout << "Why? : " << str<< endl;
+		}
+		return 0;
+	}
